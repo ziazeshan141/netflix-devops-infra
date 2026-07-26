b77e7506dd51d28884c57f2c943e2a17a9092b02
ziazeshan141 <ziazeshan141@outlook.com>
2026-07-27 00:01:25 +0300

    Added missing modules/ecr/outputs.tf

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,7 +1,9 @@
 output "frontend_repository_url" {
+  description = "The URL of the frontend ECR repository"  
   value = aws_ecr_repository.frontend.repository_url
 }
 
 output "backend_repository_url" {
+  description = "The URL of the backend ECR repository"
   value = aws_ecr_repository.backend.repository_url
 }
