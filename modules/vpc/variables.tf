0bf9738dab6506b79dc9ddd6315f22afb82ecfec
ziazeshan141 <ziazeshan141@outlook.com>
2026-07-26 14:18:49 +0300

    Updated backend.tf and modules/vpc/variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -7,3 +7,8 @@ variable "private_subnet_1_cidr" {}
 variable "private_subnet_2_cidr" {}
 variable "availability_zone_1" {}
 variable "availability_zone_2" {}
+variable "additional_tags" {
+  description = "Additional tags to apply to VPC resources"
+  type        = map(string)
+  default     = {}
+}
